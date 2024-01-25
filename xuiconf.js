96dd65fa39da89eff79dd7c7e62f92ef43a4fe73
romainbaker <63711491+romainbaker@users.noreply.github.com>
2024-01-25 09:58:45 +0100

    Updated by CrossUI No-Code App Builder

--- xuiconf.js
+++ xuiconf.js
@@ -1,6 +1,48 @@
 
+
 // [[Page Appearance
-//xui.ini.$PageAppearance = {
-//    "theme":"webflat"
-//};
+xui.ini.$PageAppearance = {
+    "theme" : "default"
+};
 // ]]Page Appearance
+
+
+// [[Font Icons CDN
+
+// ]]Font Icons CDN
+
+
+// [[Develop Env Setting
+xui.ini.$DevEnv = {
+    "designViewConf" : {
+        "width" : 800,
+        "height" : 600
+    }
+};
+// ]]Develop Env Setting
+
+
+// [[Global Functions
+xui.$cache.functions = {
+    "test" : {
+        "desc" : "",
+        "params" : [
+            {
+                "id" : "num1",
+                "type" : "Number",
+                "desc" : ""
+            },
+            {
+                "id" : "num2",
+                "type" : "Number",
+                "desc" : ""
+            }
+        ],
+        "actions" : [
+            function(num1, num2){
+                return num1 + num2;
+            }
+        ]
+    }
+};
+// ]]Global Functions
